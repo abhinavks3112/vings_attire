c8c34d5aa8a13c694c49b5566c92ac758a54ba17
Abhinav <abhiks3112@gmail.com>
2021-04-30 02:31:46 +0530

    Zoom when hovering over element, effect added

--- src/components/menu-items/menu-items.component.jsx
+++ src/components/menu-items/menu-items.component.jsx
@@ -4,9 +4,15 @@ import './menu-items.style.scss';
 const MenuItem = ({ title, imageUrl, size }) => {
     // `${size} menu-item` if present size then the value of this variable gets added as css class name, to which we can assign attributes/property in css file
     return(
-        <div style={{backgroundImage: `url(${imageUrl})`}} className={`${size} menu-item`}>
+        <div className={`${size} menu-item`}>
+            <div 
+            className='background-image' 
+            style={{
+                backgroundImage: `url(${imageUrl})`
+            }} 
+            />
             <div className="content">
-                <h1 className="title">{title}</h1>
+                <h1 className="title">{title.toUpperCase()}</h1>
                 <span className="subtitle">SHOP NOW</span>
             </div>
         </div>
